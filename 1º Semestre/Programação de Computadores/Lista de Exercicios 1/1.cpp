c334c5905d8c5f5edbfef073bcc2cafda58f30ab
Marcos Derek <49729245+MarcosDerek@users.noreply.github.com>
2022-03-16 16:58:23 -0300

    Add files via upload

---
+++ 1º Semestre/Programação de Computadores/Lista de Exercicios 1/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+//1. Escreva um programa que apresente na tela a frase: "Meu primeiro programa!!!"
+
+int main(){
+	printf("Meu primeiro programa!!!");
+}
---
+++ 1º Semestre/Programação de Computadores/Lista de Exercicios 1/14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*14. Escreva um programa que solicite ao usuário dois números reais e ao final apresente na
+tela o produto dos dois números informados da seguinte forma: "O produto dos numeros N
+e X corresponde a Y"*/
+
+int main(){
+	float numero1, numero2;
+	
+	printf("Digite o primeiro numero real: ");
+	scanf("%f", &numero1);
+	
+	printf("Digite o segundo numero real: ");
+	scanf("%f", &numero2);
+	
+	printf("O produto dos numeros %0.03f e %0.03f corresponde a %0.03f", numero1, numero2, numero1 * numero2);
+}
