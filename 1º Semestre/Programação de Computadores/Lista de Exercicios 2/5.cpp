ad9325da5dceac8ce863a99b2d27f69f856813c0
Marcos Derek <49729245+MarcosDerek@users.noreply.github.com>
2022-03-16 20:48:05 -0300

    Update 5.cpp

--- 1º Semestre/Programação de Computadores/Lista de Exercicios 2/5.cpp
+++ 1º Semestre/Programação de Computadores/Lista de Exercicios 2/5.cpp
@@ -7,7 +7,7 @@ int main(){
 	printf("digite um numero inteiro: ");
 	scanf("%i", &numero);
 	
-	if(!(numero % 3)){
+	if(numero % 3 == 0){
 		printf("O numero %i que voce informou e MULTIPLO de 3", numero);
 	} else {
 		printf("O numero %i que voce informou NAO e MULTIPLO de 3", numero);
