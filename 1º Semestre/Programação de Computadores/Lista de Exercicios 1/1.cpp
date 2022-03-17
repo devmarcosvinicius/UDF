7fecb79e6a35d909f51dd7e492c306736aba8d03
Marcos Derek <49729245+MarcosDerek@users.noreply.github.com>
2022-03-17 00:34:46 -0300

    Delete 1º Semestre/Programação de Computadores/Lista de Exercicios 1 directory

--- 1º Semestre/Programação de Computadores/Lista de Exercicios 1/1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-//1. Escreva um programa que apresente na tela a frase: "Meu primeiro programa!!!"
-
-int main(){
-	printf("Meu primeiro programa!!!");
-}
