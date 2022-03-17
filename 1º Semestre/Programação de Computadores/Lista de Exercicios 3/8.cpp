1267e47bb68a077e530dd149f44a9c836c9c20e0
Marcos Derek <49729245+MarcosDerek@users.noreply.github.com>
2022-03-16 21:18:58 -0300

    Add files via upload

---
+++ 1º Semestre/Programação de Computadores/Lista de Exercicios 3/8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+/*8. Elabore um algoritmo que imprima todos os números ímpares de 1000 até 0.*/
+
+int main(){
+	for(int i = 1000; i >= 0; i--){
+		cout << i << "\n";
+	}
+}
