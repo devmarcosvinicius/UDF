13e5acf7bb7e2cae74b98006e5a873e08ce57be9
Marcos Derek <49729245+MarcosDerek@users.noreply.github.com>
2022-04-04 02:45:11 -0300

    Add files via upload

---
+++ 1º Semestre/Programação de Computadores/Lista de Exercicios 1/Python/Lista de exercicios 01/04.py
@@ -0,0 +1,8 @@
+"""
+4.	Escreva um programa que solicite ao usuário dois números inteiros e ao final apresente na tela 
+os dois números informados da seguinte forma: "Voce informou os numeros X e Y" 
+"""
+
+IntNum1 = int(input("Digite o primeiro numero inteiro: "))
+intNum2 = int(input("Digite o segundo numero inteiro: "))
+print("Você informou os numeros", IntNum1, "e", intNum2)
