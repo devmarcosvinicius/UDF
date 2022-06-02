bb55aa7d028b8f09c057cfab8f6172ed737c3368
Marcos Derek <49729245+MarcosDerek@users.noreply.github.com>
2022-06-01 22:29:50 -0300

    Add files via upload

--- 1º Semestre/Programação de Computadores/Simulado/3.py
+++ 1º Semestre/Programação de Computadores/Simulado/3.py
@@ -3,17 +3,14 @@ Faça um programa que leia seis valores numéricos atribuindo-os à duas variáv
 lista com três elementos cada. Cada variável irá representar um vetor, informe o produto 
 escalar e o produto vetorial destes vetores. 
 """
-import numpy
-
 lista3 = []
 lista4 = []
 
 for x in range(3):
-  userInput = int(input("Digite um numero: "))
-  lista3.append(userInput)
+  lista3.append(int(input("Digite um numero para primeira lista: ")))
   
 for x in range(3):
-  userInput = int(input("Digite um numero: "))
-  lista4.append(userInput)
+  lista4.append(int(input("Digite um numero para segunda lista: ")))
 
-numpy.cross(lista3, lista4)
+sum(lista3 + lista4) * 2 
+lista3 + lista4
